b052fb89c712b794267a981e4463431bb7591c20
lalith4varma <lalithvarma666@gmail.com>
2025-04-30 00:10:29 +0530

    Update site for latest changes

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <header className="bg-gray-800 text-gray-50 py-4 px-4">
-      <nav className="flex justify-center items-center max-w-7xl mx-auto w-full">
-        <ul className="flex space-x-16 text-lg">
+      <nav className="flex justify-center items-center max-w-6xl mx-auto">
+        <ul className="flex space-x-12 text-lg">
           <li>
             <a href="#about" className="hover:text-green-400">
               About
